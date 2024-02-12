ee0a0b06e3c4b25d2a94944f6af1da91523c63d9
UNIT <nox@localhost.localdomain>
2024-02-12 21:22:48 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
@@ -18,6 +18,7 @@ import "./styles/locomotive-scroll.css";
 // import 'locomotive-scroll/dist/locomotive-scroll.css'
 
 const App = () => {
+
   return (
     <>
       <BrowserRouter>
